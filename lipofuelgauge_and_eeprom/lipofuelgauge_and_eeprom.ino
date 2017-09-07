4bd754f4367e7c7908c4c622a7e0015c523e6bfb
davidmaelfait <davidmaelfait@hotmail.com>
2017-09-07 21:56:34 +0200

    serial added

--- lipofuelgauge_and_eeprom/lipofuelgauge_and_eeprom.ino
+++ lipofuelgauge_and_eeprom/lipofuelgauge_and_eeprom.ino
@@ -1,9 +1,34 @@
-void setup() {
-  // put your setup code here, to run once:
+#define ESP8266_LED 5
 
+String str;
+
+void setup() 
+{
+  pinMode(ESP8266_LED, OUTPUT);
+
+  Serial.begin(9600);
+  
+  while(!Serial)
+    ;
+
+  Serial.println("serial port ready");
+
+    digitalWrite(ESP8266_LED, HIGH);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
 
+
+  if(Serial.available() > 0)
+    {
+        if(Serial.findUntil("ssid=","\n"))
+        {
+        
+        str = Serial.readStringUntil('\n');
+        str.replace("ssid=", "");
+        str.replace("\n", "");
+        Serial.print("rx: " +str);
+        }
+    }
 }

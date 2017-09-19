fdd966075c31a43072e8d99556c1d87e08096d00
davidmaelfait <davidmaelfait@hotmail.com>
2017-09-19 20:51:00 +0200

    2nd try for cli

--- lipofuelgauge_and_eeprom/lipofuelgauge_and_eeprom.ino
+++ lipofuelgauge_and_eeprom/lipofuelgauge_and_eeprom.ino
@@ -1,34 +1,62 @@
+
+#include <CLI.h>
 #define ESP8266_LED 5
 
-String str;
+//String str;
+
+CLI_COMMAND(ssidFunc) {
+    if (argc != 1) {
+        dev->println("Usage: ssid <ssid>");
+        return 10;
+    }
+
+    dev->println(argv[1]);
+
+    return 0;
+}
+
+
+CLI_COMMAND(connectFunc) {
+    dev->println("Welcome to the crius alarm sensor.");
+    dev->println();
+    dev->printPrompt();
+}
+
 
 void setup() 
 {
-  pinMode(ESP8266_LED, OUTPUT);
+//  pinMode(ESP8266_LED, OUTPUT);
+//
+//  Serial.begin(9600);
+//  
+//  while(!Serial)
+//    ;
+//
+//  Serial.println("serial port ready");
+//
+//    digitalWrite(ESP8266_LED, HIGH);
+    Serial.begin(9600);
+    
+    CLI.setDefaultPrompt("> ");
+    CLI.onConnect(connectFunc);
+    
+    CLI.addCommand("ssid", ssidFunc);
 
-  Serial.begin(9600);
-  
-  while(!Serial)
-    ;
-
-  Serial.println("serial port ready");
-
-    digitalWrite(ESP8266_LED, HIGH);
+     CLI.addClient(Serial);
 }
 
 void loop() 
 {
-
-
-  if(Serial.available() > 0)
-    {
-        if(Serial.findUntil("ssid=","\n"))
-        {
-        
-        str = Serial.readStringUntil('\n');
-        str.replace("ssid=", "");
-        str.replace("\n", "");
-        Serial.print("rx: " +str);
-        }
-    }
+CLI.process();
+//  if(Serial.available() > 0)
+//    {
+//        if(Serial.findUntil("ssid=","\n"))
+//        {
+//        
+//        str = Serial.readStringUntil('\n');
+//        str.replace("ssid=", "");
+//        str.replace("\n", "");
+//        Serial.print("rx: " +str);
+//        }
+//    }
 }
---
+++ lipofuelgauge_and_eepromV2/lipofuelgauge_and_eepromV2.ino
@@ -0,0 +1,12 @@
+  #include "CommandLine.h"
+
+  void
+  setup() {
+  Serial.begin(115200);
+  }
+
+  void
+  loop() {
+  bool received = getCommandLineFromSerialPort(CommandLine);      //global CommandLine is defined in CommandLine.h
+  if (received) DoMyCommand(CommandLine);
+  }
